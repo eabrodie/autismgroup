d0cf2f91f1f015bad9596391b92cee38c0ae87f1
Eleanor Brodie <eleanorbrodie@Eleanors-MacBook-Pro.local>
2016-04-19 19:27:29 +0100

    Added example 4
    Added new page style

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ app.get('/main.jpg', function (req, res, next) {
   res.sendFile(__dirname + '/images/jump.jpg');
 });
 
+app.get('/logo-teal.jpg', function (req, res, next) {
+  res.sendFile(__dirname + '/example4/images/logo-teal.jpg');
+});
+
+app.get('/child.jpg', function (req, res, next) {
+  res.sendFile(__dirname + '/example4/images/child.jpg');
+});
+
 function serveExample(exampleName) {
 
   app.get(exampleName + '/', function (req, res, next) {
@@ -36,5 +44,6 @@ function serveExample(exampleName) {
 serveExample('/example1');
 serveExample('/example2');
 serveExample('/example3');
+serveExample('/example4');
 
 module.exports = app.listen(3000);
